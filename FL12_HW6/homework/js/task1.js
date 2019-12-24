2b71b81e477c233ef7688e3240cad0eacb3f1222
AndriiStefanyuk <astefanyuk@ukr.net>
2019-12-24 19:49:31 +0200

    Intro to JS_HT

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,37 @@
+let a = prompt(
+  'Let\'s try to solve Quadratic equation ax2 + bx + c = 0. Enter value for "a", it should be numeric value but not 0',
+  ""
+);
+let b = prompt('Enter value for "b", it should be numeric value', "");
+let c = prompt('Enter value for "c", it should be numeric value', "");
+
+if (
+  a === "" ||
+  a === null ||
+  b === "" ||
+  b === null ||
+  c === "" ||
+  c === null
+) {
+  console.log("Invalid input data");
+} else {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  c = parseFloat(c);
+  let D, x, x1, x2;
+  if (a === 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log("Invalid input data");
+  } else {
+    D = b * b - (a * c + a * c + a * c + a * c);
+    if (D === 0) {
+      x = -b / (a + a);
+      console.log("x=" + Math.round(x));
+    } else if (D > 0) {
+      x1 = (-b + D) / (a + a);
+      x2 = (-b - D) / (a + a);
+      console.log("x1=" + Math.round(x1) + " and " + "x2=" + Math.round(x2));
+    } else if (D < 0) {
+      console.log("no solution");
+    }
+  }
+}
---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,42 @@
+let a = prompt(
+  'Let\'s try to identify triangle type. Enter length for "a" triangle side',
+  ""
+);
+let b = prompt('Enter length for "b" triangle side', "");
+let c = prompt('Enter length for "c" triangle side', "");
+
+if (
+  a === "" ||
+  a === null ||
+  b === "" ||
+  b === null ||
+  c === "" ||
+  c === null
+) {
+  alert("input values should be ONLY numbers");
+} else {
+  a = parseInt(a);
+  b = parseInt(b);
+  c = parseInt(c);
+  if (
+    a === 0 ||
+    a < 0 ||
+    isNaN(a) ||
+    b === 0 ||
+    b < 0 ||
+    isNaN(b) ||
+    c === 0 ||
+    c < 0 ||
+    isNaN(c)
+  ) {
+    alert("A triangle must have 3 sides with a positive definite length");
+  } else if (a + b <= c || a + c <= b || c + b <= a) {
+    alert("Triangle doesn’t exist");
+  } else if (a === b && b === c) {
+    alert("Equilateral triangle");
+  } else if (a === b || b === c || a === c) {
+    alert("Isosceles triangle");
+  } else {
+    alert("Scalene triangle");
+  }
+}
